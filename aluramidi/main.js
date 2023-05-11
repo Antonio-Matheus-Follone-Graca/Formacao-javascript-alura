2b220226fb94844b62409cc0bbf52964e3fc8a25
Antônio Matheus Follone Graça <98711863+Antonio-Matheus-Follone-Graca@users.noreply.github.com>
2023-05-10 23:55:42 -0300

    Revert "Funções de mudar classe dos botões e validações"
    This reverts commit d030577f833becf571eabd4d009064c989dcdc31.

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -6,21 +6,8 @@ function tocar_audio(id){
     /* outra forma de executar o aúdio
     audio = document.querySelector('#'+id)
     audio.play() ou audio.play(audio.src) src é o caminho do aúdio*/
-    // refatorando funcao para usar em listas fora da função e de acordo com o vídeo do curso
-    const elemento = document.querySelector(id);
-    // validações
-    if (elemento === null) {
-        console.log('Elemento não encontrado')
-    }
-
-    // se a tag do elemento não for aúdio, dará erro na hora de executar o aúdi
-    if (elemento != null &&  elemento.localName === 'audio') {
-        elemento.play()
-    }
-    else{
-        console.log('Elemento não encontrado ou seletor inválido')
-    }
-   
+    // refatorando funcao para usar em listas fora da função
+    document.querySelector(id).play();
    
 }
 
@@ -36,22 +23,6 @@ for (let contador = 0 ;contador < ListaDeTeclas.length;contador ++) {
     // usando função anonônima
     tecla.onclick= function(){
         tocar_audio(idAudio);
-    }
-    // adicionando outro evento, ao clicar muda a cor de fundo
-    tecla.onkeydown = function(evento){
-        console.log(evento.code)  // pegando a tecla do teclado
-
-        // se  o usuário apertou essas teclas abaixo, adiciona a classe ativa
-        if (evento.code === 'Space' || evento.code ==='Enter')
-        {
-            // adicionando classe 
-            ListaDeTeclas[contador].classList.add('ativa')
-        }
-       
-    }
-    // remove classe ao soltar a tecla
-    tecla.onkeyup = function () {
-        ListaDeTeclas[contador].classList.remove('ativa')
     }
    
 }
