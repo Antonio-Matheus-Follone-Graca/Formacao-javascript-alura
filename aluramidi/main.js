02673dd10c48bedb429218c533bd00b0198c4248
Antônio Matheus Follone Graça <98711863+Antonio-Matheus-Follone-Graca@users.noreply.github.com>
2023-05-10 23:58:44 -0300

    Update main.js

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -6,8 +6,26 @@ function tocar_audio(id){
     /* outra forma de executar o aúdio
     audio = document.querySelector('#'+id)
     audio.play() ou audio.play(audio.src) src é o caminho do aúdio*/
+<<<<<<< HEAD
     // refatorando funcao para usar em listas fora da função
     document.querySelector(id).play();
+=======
+    // refatorando funcao para usar em listas fora da função e de acordo com o vídeo do curso
+    const elemento = document.querySelector(id);
+    // validações
+    if (elemento === null) {
+        console.log('Elemento não encontrado')
+    }
+
+    // se a tag do elemento não for aúdio, dará erro na hora de executar o aúdi
+    if (elemento != null &&  elemento.localName === 'audio') {
+        elemento.play()
+    }
+    else{
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
+   
+>>>>>>> parent of 911f301 (Criando pasta formacao-javascript-alura)
    
 }
 
@@ -24,5 +42,24 @@ for (let contador = 0 ;contador < ListaDeTeclas.length;contador ++) {
     tecla.onclick= function(){
         tocar_audio(idAudio);
     }
+<<<<<<< HEAD
+=======
+    // adicionando outro evento, ao clicar muda a cor de fundo
+    tecla.onkeydown = function(evento){
+        console.log(evento.code)  // pegando a tecla do teclado
+
+        // se  o usuário apertou essas teclas abaixo, adiciona a classe ativa
+        if (evento.code === 'Space' || evento.code ==='Enter')
+        {
+            // adicionando classe 
+            ListaDeTeclas[contador].classList.add('ativa')
+        }
+       
+    }
+    // remove classe ao soltar a tecla
+    tecla.onkeyup = function () {
+        ListaDeTeclas[contador].classList.remove('ativa')
+    }
+>>>>>>> parent of 911f301 (Criando pasta formacao-javascript-alura)
    
 }
