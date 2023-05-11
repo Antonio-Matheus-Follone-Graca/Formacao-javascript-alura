0d1d0ea1620461725cf1ec897aa9cb9a5a123d29
Antônio Matheus Follone Graça <98711863+Antonio-Matheus-Follone-Graca@users.noreply.github.com>
2023-05-11 01:26:02 -0300

    Update main.js

--- modulo2-robotron-2000-projeto_inicial/js/main.js
+++ modulo2-robotron-2000-projeto_inicial/js/main.js
@@ -2,16 +2,21 @@ const robotron = document.querySelector('#robotron')
 
 // invocando função
 // addEventListener espera um evento para executá-lo. Nesse caso no evento click, executa a função dizOi
-robotron.addEventListener('click',dizOI)
 
-function dizOI(){
-    console.log('oi')
+// arrow function, função anônima
+robotron.addEventListener('click',(evento) =>{
+    // parametro evento mostrará tudo no console sobre o click
+    console.log(evento)
+})
+
+function dizOI(nome){
+    console.log('oi '+ nome)
+    console.log('Seja bem vindo ao robotron 2000')
     // outra forma seria 
     /*document.querySelector('#robotron').onclick = function() {
         console.log('outra forma de usar o evento click')
     }*/
 
 }
-
-//dizOI()
+dizOI("Pedro")
     
