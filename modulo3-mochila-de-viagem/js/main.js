919bb7ceba58fee388623d6961521f783a5eac28
Antônio Matheus Follone Graça <98711863+Antonio-Matheus-Follone-Graca@users.noreply.github.com>
2023-05-13 22:49:16 -0300

    Salvando dados no local storage
    E refatorando o código

--- modulo3-mochila-de-viagem/js/main.js
+++ modulo3-mochila-de-viagem/js/main.js
@@ -1,44 +1,52 @@
 // guardando formulário numa variável
-const formulario =document.getElementById('novoItem');
-const lista = document.getElementById('lista')
+const form = document.getElementById("novoItem") 
+const lista = document.getElementById("lista")
 // array para poder inserir várias informações em um mesmo objeto no local storage
-const itens = [] 
-// evento de submit é para quando envia o formulário
-formulario.addEventListener('submit',(evento) =>{
-    evento.preventDefault(); // Cancela o evento se for cancelável, sem parar a propagação do mesmo. No caso do evento submit, ele recarrega a página, por isso o preventdefault para evitar de recarregar a  página
-    console.log(evento);
-    // pegando  valores dos campos
-    const nome = evento.target.elements['nome'].value
-    const quantidade = evento.target.elements['quantidade'].value
-    criaElemento(nome,quantidade)
-    // limpando valores do formulário após o submit
-    nome.value =""
-    quantidade.value =""
-})
+//pega os dados do local storage de name itens, caso esteja vázio cria um local storage
+const itens = JSON.parse(localStorage.getItem("itens")) || []  
 
-function criaElemento(nome,quantidade){
-    // criando um elemento, uma tag
-    // <li class="item"><strong>7</strong>Camisas</li>
-    const novoItem = document.createElement('li')
-    novoItem.classList.add('item')
-    const numeroItem = document.createElement('strong')
-    // innerHTML é o conteudo entre as tags, exemplo <strong> 5 </strong> o 5 é o conteúdo do innerHTMl
-    numeroItem.innerHTML = quantidade
-    // adicionando na tag li, a tag strong, ficando <li class="item"> <strong> quantidade </strong> </li>
-    novoItem.appendChild(numeroItem) 
-    // adicionando o nome 
-    novoItem.innerHTML += nome
-    // adicionando a lista a variavel novoItem
-    lista.appendChild(novoItem)
+itens.forEach( (elemento) => {    
+    criaElemento(elemento)
+    console.log(elemento)
+} )
+
+// evento de submit é para quando envia o formulári
+form.addEventListener("submit", (evento) => {  
+    evento.preventDefault()// Cancela o evento se for cancelável, sem parar a propagação do mesmo. No caso do evento submit, ele recarrega a página, por isso o preventdefault para evitar de recarregar a  página
+    //console.log(evento);
+    // pegando  valores dos campos
+
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
     // objeto para o local storage
     const itemAtual = {
-        'nome':nome,
-        'quantidade': quantidade
+    "nome": nome.value,
+    "quantidade": quantidade.value
     }
+
+    criaElemento(itemAtual)
+
     itens.push(itemAtual)
     // usando localStorage para salvar dados no navegador
     // local storage só aceita string, por isso o JSON.stringify
-    localStorage.setItem('item',JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens))
+    // limpando valores do formulário após o submit
+    nome.value = ""
+    quantidade.value = ""
+})
 
-
+function criaElemento(item) {  
+    // criando um elemento, uma tag
+    // <li class="item"><strong>7</strong>Camisas</li>
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+    const numeroItem = document.createElement('strong')
+    // innerHTML é o conteudo entre as tags, exemplo <strong> 5 </strong> o 5 é o conteúdo do innerHTMl
+    numeroItem.innerHTML = item.quantidade
+    // adicionando na tag li, a tag strong, ficando <li class="item"> <strong> quantidade </strong> </li>
+    novoItem.appendChild(numeroItem)
+    // adicionando o nome 
+    novoItem.innerHTML += item.nome
+    // adicionando a lista a variavel novoItem
+    lista.appendChild(novoItem)
 }
