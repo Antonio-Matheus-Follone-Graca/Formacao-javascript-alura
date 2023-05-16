b53deeebc66c6c255ac14b259bb44bcdc72d3458
Antônio Matheus Follone Graça <98711863+Antonio-Matheus-Follone-Graca@users.noreply.github.com>
2023-05-16 19:27:27 -0300

    Preparando projeto
    Estilizando e javascript

---
+++ modulo4-validacoes e reconhecimentos de voz/app/sortear_numero.js
@@ -0,0 +1,17 @@
+const menorValor = 1
+const maiorValor = 10
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio(){
+    // número entre 0 e 100 
+    return parseInt (Math.random() *maiorValor +1)
+}
+
+console.log('Número secreto:',numeroSecreto)
+
+// alterando o texto do index.html nos valores
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
