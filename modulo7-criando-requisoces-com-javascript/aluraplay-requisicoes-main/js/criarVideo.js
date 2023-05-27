49fdf6b9ea217657ea1261001e97223c59b72272
Antônio Matheus Follone Graça <98711863+Antonio-Matheus-Follone-Graca@users.noreply.github.com>
2023-05-27 20:59:26 -0300

    post de um vídeo

---
+++ modulo7-criando-requisoces-com-javascript/aluraplay-requisicoes-main/js/criarVideo.js
@@ -0,0 +1,25 @@
+// importando arquivo conectaApi.js 
+import { conectaApi } from "./conectaApi.js";
+
+// acessando data atribute de formulário
+const formulario = document.querySelector('[data-formulario]');
+
+// função assincrona pois ela chama um método assincrono do  conectaApi.js
+async function criarVideo(evento) {
+    // impedindo de atualizar a página
+    evento.preventDefault();
+    // pegando os valores dos campos do formulário
+    const imagem = document.querySelector('[data-imagem]').value;
+    const url = document.querySelector('[data-url]').value;
+    const titulo = document.querySelector('[data-titulo]').value;
+    // descrição seu valor será aleatório pois não possui um contador de visualizações o projeto
+    const descricao = Math.floor(Math.random()*10).toString();
+    // conectando a api e chamando a funcao cria video do conectaApi.js
+    await conectaApi.criaVideo(titulo,descricao,url,imagem);
+    // redirecionando página
+    window.location.href = '../pages/envio-concluido.html'
+
+}
+
+// ao evento de submit do formulário
+formulario.addEventListener('submit', evento => criarVideo(evento));
